150141120f59b72045c011d98bc9ecf032b79e2e
Pratik Singh <61048532+imsine0@users.noreply.github.com>
2021-04-07 02:09:47 +0530

    Update sudoku_solver.py

--- sudoku_solver.py
+++ sudoku_solver.py
@@ -16,7 +16,7 @@ def print_board():
         if i % 3 == 0 and i != 0:
             print("- - - - - - - - - - - - ")
 
-        for j in range(len(board)):
+        for j in range(len(board[0])):
             if j % 3 == 0 and j != 0:
                 print(" | ", end="")
 
@@ -30,7 +30,7 @@ def print_board():
 # (i , j)  row , col
 def find_empty(board):
     for i in range(len(board)):
-        for j in range(len(board)):
+        for j in range(len(board[0])):
             if board[i][j] == 0:
                 return i, j
     return None
@@ -40,7 +40,7 @@ def find_empty(board):
 
 def check(board, num, pos):
     # check row
-    for i in range(len(board)):
+    for i in range(len(board[0])):
         if board[pos[0]][i] == num and pos[1] != i:
             return False
 
@@ -53,8 +53,8 @@ def check(board, num, pos):
     box_i = pos[1] // 3
     box_j = pos[0] // 3
 
-    for i in range(box_j * 3, box_j * 3, +3):
-        for j in range(box_i * 3, box_i * 3, +3):
+    for i in range(box_j * 3, box_j * 3 + 3):
+        for j in range(box_i * 3, box_i * 3 + 3):
             if board[i][j] == num and (i, j) != pos:
                 return False
     return True
@@ -82,3 +82,6 @@ def solver(board):
             board[row][col] = 0
     return False
 
+
+solver(board)
+print_board()
